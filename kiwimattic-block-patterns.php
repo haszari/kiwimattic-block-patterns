b4813d917d135d82a4b5248e1f58f8085e72d088
haszari <haszari@cartoonbeats.com>
2021-07-19 10:48:41 +1200

    wp scaffold plugin kiwimattic-block-patterns

---
+++ kiwimattic-block-patterns.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Kiwimattic Block Patterns
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     kiwimattic-block-patterns
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Kiwimattic_Block_Patterns
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Kiwimattic_Block_Patterns
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
